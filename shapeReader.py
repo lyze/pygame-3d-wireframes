2df02da5d4ec985182a2c7257fac19e62aeb32a6
Francesco Melpignano <melpignanof@gmail.com>
2014-12-16 05:16:50 +0000

    shapeReader.py edited online with Bitbucket

--- shapeReader.py
+++ shapeReader.py
@@ -1,7 +1,14 @@
 '''
  reads in text file with edge coordinates and optional color field
- returns a list of lists of edges with coordinates and color
+ returns a shape as a list of Edges
+ text file has the following format on each line:
+ start point, end point, color(optional)
+ 0 0 0, 1 0 0, 0xFFF000
 '''
+
+from three_d.primitives.edge import Edge
+import numpy as np
+
 
 class ShapeReader(object):
 
@@ -10,9 +17,15 @@ class ShapeReader(object):
 
     def process_file(self):
         with open(self.shape_file, 'r') as f:
-            edges = []
+            shape = []
             for line in f:
                 line = line.strip()
                 edge = line.split(',')
-                egdes.append(edge)
-            return edges
+                #if no color provided defaults it to black
+                if len(edge) == 2:
+                    edge.append('0xFFFFFF')
+                shape.append(Edge(np.array(list(edge[0]))), Edge(np.array(list(edge[1]))),
+                             Edge(np.array(list(edge[2]))))
+                return shape
+            
+            
